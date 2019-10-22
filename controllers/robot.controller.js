b2d59d7ebb2bfaf3efd1a4ec7288fea3e63ac49c
faisal.ihsanulfikri <faisalihsanulfikri@gmail.com>
2019-10-22 13:43:14 +0700

    RHB LOGIN

--- controllers/robot.controller.js
+++ controllers/robot.controller.js
@@ -2,29 +2,55 @@ const { Security } = require("../models");
 const { Robot } = require("../models");
 const { to, ReE, ReS } = require("../services/util.service");
 
-// function create security
 module.exports.run = async function(req, res) {
-  const CDP = require("chrome-remote-interface");
+  const puppeteer = require("puppeteer");
+  const Tesseract = require("../node_modules/tesseract.js");
+  const fs = require("fs");
+  const username = "Rvr2492";
+  const password = "147903";
 
-  CDP(client => {
-    // Extract used DevTools domains.
-    const { Page, Runtime } = client;
-
-    // Enable events on domains we are interested in.
-    Promise.all([Page.enable()]).then(() => {
-      return Page.navigate({ url: "http://mainapi.berkreatifitas.com" });
-    });
-
-    // Evaluate outerHTML after page has loaded.
-    Page.loadEventFired(() => {
-      Runtime.evaluate({ expression: "document.body.outerHTML" }).then(
-        result => {
-          console.log(result.result.value);
-          client.close();
-        }
-      );
-    });
-  }).on("error", err => {
-    console.error("Cannot connect to browser:", err);
+  const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: null
   });
+
+  const URL = "https://webtrade.rhbtradesmart.co.id/onlineTrading/login.jsp";
+  const page = await browser.newPage();
+
+  await page.goto(URL);
+  await page.type("input[id='j_username']", username);
+  await page.type("input[id='password']", password);
+
+  await page.waitForSelector("img[alt='athentication token']");
+  await page.evaluate(() => {
+    document.querySelector(".form-login").style.backgroundColor = "white";
+    document.querySelector("img[alt='athentication token']").style.transform =
+      "scale(2.2) skew(-60deg, 5deg)";
+    document.querySelector("img[alt='athentication token']").style.filter =
+      "grayscale(1) brightness(3) contrast(10)";
+  });
+
+  let captcha = await page.$("img[alt='athentication token']");
+  await captcha.screenshot({
+    path: "./public/images/captcha/captcha.png",
+    omitBackground: true
+  });
+
+  let tokenCaptcha = await Tesseract.recognize(
+    "./public/images/captcha/captcha.png",
+    "eng",
+    {
+      logger: m => console.log(m)
+    }
+  ).then(({ data: { text } }) => {
+    token = text.replace(/\D+/g, "").trim();
+    token.toString().substring(0, 4);
+    return token;
+  });
+
+  console.log("token ", tokenCaptcha);
+
+  await page.type("input[id='j_token']", tokenCaptcha);
+
+  await page.click("button[type=submit]");
 };
