96a132279c9126051634a786f7a4a2de2c21cb07
Andri Firmansyah <andrifmsyh@gmail.com>
2019-10-23 09:53:26 +0700

    Create new api for getting Stocks data from Google Spreadsheets

---
+++ routes/spreadsheet.js
@@ -0,0 +1,37 @@
+const { promisify } = require("util")
+const express = require('express')
+const GoogleSpreadsheet = require("google-spreadsheet")
+
+const router = express.Router()
+const cred = require("../client_secret.json")
+
+router.get('/', async (req, res) => {
+  const id = "13ESpdzifw98ptByFFJqNKN316tnEiMCYcTsquSTsYtM"
+  const doc = new GoogleSpreadsheet(id)
+
+  await promisify(doc.useServiceAccountAuth)(cred)
+
+  const info = await promisify(doc.getInfo)()
+  const sheet = info.worksheets[0]
+  const rows = await promisify(sheet.getRows)({ offset: 1 })
+
+  let stocks = []
+
+  rows.forEach(el => {
+    stocks.push({
+      no: el.no,
+      stock: el.stock,
+      close: el.close,
+      volume: el.volume,
+      value: el.value
+    })
+  })
+
+  return res.json({
+    method: req.method,
+    length: stocks.filter(el => el.stock !== '').length,
+    data: stocks.filter(el => el.stock !== '')
+  })
+})
+
+module.exports = router
