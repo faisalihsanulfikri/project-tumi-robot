d1a5fa4bcc9ef73fed1e6a709e56603791a3d6eb
Andri Firmansyah <andrifmsyh@gmail.com>
2019-10-31 15:32:02 +0700

    Scraping data transaction/order status

--- controllers/transaction.controller.js
+++ controllers/transaction.controller.js
@@ -1,57 +1,58 @@
-const { to, ReE, ReS } = require('../services/util.service');
-const { Transaction } = require("../models");
-const { Stock } = require("../models");
+const { to, ReE, ReS } = require("../services/util.service")
+const { Transaction } = require("../models")
+const { Stock } = require("../models")
 
 module.exports.get_transaction = async function(req, res) {
-    let transaction;
-    let stock;
-    let transactions
-  
-    [err, transaction] = await to(Transaction.findAll({ raw: true }));
-  
-    transaction.forEach(el => {
-      transactions = el;
-    });
-    [err, stock] = await to(Stock.findOne({where: { id: transactions.stock_id }}));
-  
-  
-    return ReS(res, { transaction: transactions, Stock: stock });
-  };
+  let transaction
+  let stock
+  let transactions
+  ;[err, transaction] = await to(Transaction.findAll({ raw: true }))
+
+  transaction.forEach(el => {
+    transactions = el
+  })
+  ;[err, stock] = await to(
+    Stock.findOne({ where: { id: transactions.stock_id } })
+  )
+
+  return ReS(res, { transaction: transactions, Stock: stock })
+}
 
 module.exports.buyAndSell = async function(req, res) {
-
-  const puppeteer = require("puppeteer");
-  const Tesseract = require("../node_modules/tesseract.js");
-  const fs = require("fs");
-  const username = "Rvr2492";
-  const password = "147903";
+  const puppeteer = require("puppeteer")
+  const Tesseract = require("../node_modules/tesseract.js")
+  const fs = require("fs")
+  const username = "Rvr2492"
+  const password = "147903"
 
   const browser = await puppeteer.launch({
     headless: false,
-    defaultViewport: null
-  });
+    defaultViewport: null,
+    executablePath:
+      "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
+  })
 
-  const URL = "https://webtrade.rhbtradesmart.co.id/onlineTrading/login.jsp";
-  const page = await browser.newPage();
+  const URL = "https://webtrade.rhbtradesmart.co.id/onlineTrading/login.jsp"
+  const page = await browser.newPage()
 
-  await page.goto(URL);
-  await page.type("input[id='j_username']", username);
-  await page.type("input[id='password']", password);
+  await page.goto(URL)
+  await page.type("input[id='j_username']", username)
+  await page.type("input[id='password']", password)
 
-  await page.waitForSelector("img[alt='athentication token']");
+  await page.waitForSelector("img[alt='athentication token']")
   await page.evaluate(() => {
-    document.querySelector(".form-login").style.backgroundColor = "white";
+    document.querySelector(".form-login").style.backgroundColor = "white"
     document.querySelector("img[alt='athentication token']").style.transform =
-      "scale(2.2) skew(-60deg, 5deg)";
+      "scale(2.2) skew(-60deg, 5deg)"
     document.querySelector("img[alt='athentication token']").style.filter =
-      "grayscale(1) brightness(3) contrast(10)";
-  });
+      "grayscale(1) brightness(3) contrast(10)"
+  })
 
-  let captcha = await page.$("img[alt='athentication token']");
+  let captcha = await page.$("img[alt='athentication token']")
   await captcha.screenshot({
     path: "./public/images/captcha/captcha.png",
     omitBackground: true
-  });
+  })
 
   let tokenCaptcha = await Tesseract.recognize(
     "./public/images/captcha/captcha.png",
@@ -60,36 +61,59 @@ module.exports.buyAndSell = async function(req, res) {
       // logger: m => console.log(m)
     }
   ).then(({ data: { text } }) => {
-    token = text.replace(/\D+/g, "").trim();
-    token.toString().substring(0, 4);
-    return token;
-  });
+    token = text.replace(/\D+/g, "").trim()
+    token.toString().substring(0, 4)
+    return token
+  })
 
-  console.log("token ", tokenCaptcha);
+  await page.type("input[id='j_token']", tokenCaptcha)
+  await page.click("button[type=submit]")
 
-  await page.type("input[id='j_token']", tokenCaptcha);
+  await page.goto(
+    "https://webtrade.rhbtradesmart.co.id/onlineTrading/html/orderstatus.jsp"
+  )
+  await page.click("button[onclick='objPopup.showLoginTrading();']")
+  await page.type("input[id='_ltPin']", password)
+  await page.click("input[id=_ltEnter]")
+  await page.click("button[id='_orderEnter']")
 
-  await page.click("button[type=submit]");
+  await page.waitFor(1000)
 
-    await page.goto('https://webtrade.rhbtradesmart.co.id/onlineTrading/html/orderstatus.jsp');
-    await page.click("button[onclick='objPopup.showLoginTrading();']");
-    await page.type("input[id='_ltPin']", password);
-    await page.click("input[id=_ltEnter]");
-    await page.click("button[id='_orderEnter']");
-
-    await page.waitFor(1000);
-
+  try {
     const data = await page.evaluate(() => {
-      let results = [];
-      let items = document.querySelectorAll('td[class=T1_col_1]');
-      console.log('items', items);
-      items.forEach((item) => {
-          results.push({
-              text: item.innerText,
-          });
-      });
-      return results;
+      return new Promise((resolve, reject) => {
+        let table = document.querySelector("#_orderTable")
+        let row = table.children
+        let items = []
+
+        for (let index = 0; index < row.length; index++) {
+          let result = {}
+
+          for (let i = 0; i < row[index].cells.length; i++) {
+            result["time"] = row[index].cells[1].textContent
+            result["order"] = row[index].cells[2].textContent
+            result["market"] = row[index].cells[3].textContent
+            result["action"] = row[index].cells[4].textContent
+            result["stock"] = row[index].cells[5].textContent
+            result["price"] = row[index].cells[7].textContent
+            result["remain"] = row[index].cells[8].textContent
+            result["match"] = row[index].cells[9].textContent
+            result["status"] = row[index].cells[9].textContent
+            result["order_amount"] = row[index].cells[10].textContent
+            result["match_amount"] = row[index].cells[11].textContent
+            result["validity"] = row[index].cells[12].textContent
+            result["channel"] = row[index].cells[13].textContent
+          }
+
+          items.push(result)
+        }
+
+        resolve(items)
+      })
     })
-    // console.log(data);
-    return ReS(res, {data: data});
-  };
+
+    return res.json(data)
+  } catch (err) {
+    res.json({ err })
+  }
+}
