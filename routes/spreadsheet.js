f06dcc73e120dd065a400c010e8b1bf233e08b80
Andri Firmansyah <andrifmsyh@gmail.com>
2019-11-05 10:54:57 +0700

    Create new api for getting spreadsheet base on it sheet name

--- routes/spreadsheet.js
+++ routes/spreadsheet.js
@@ -5,6 +5,13 @@ const GoogleSpreadsheet = require("google-spreadsheet")
 const router = express.Router()
 const cred = require("../client_secret.json")
 
+const sheets = [
+  { index: 1, name: "defaultpukat" },
+  { index: 2, name: "ch10" },
+  { index: 3, name: "ch2" },
+  { index: 5, name: "aro" }
+]
+
 router.get("/", async (req, res) => {
   const id = "13ESpdzifw98ptByFFJqNKN316tnEiMCYcTsquSTsYtM"
   const doc = new GoogleSpreadsheet(id)
@@ -69,6 +76,44 @@ router.get("/", async (req, res) => {
     method: req.method,
     length: stocks.filter(el => el.stock !== "").length,
     data: stocks.filter(el => el.stock !== "")
+  })
+})
+
+router.get("/:sheet", async (req, res) => {
+  const id = "13ESpdzifw98ptByFFJqNKN316tnEiMCYcTsquSTsYtM"
+  const doc = new GoogleSpreadsheet(id)
+  const sheetName = req.params.sheet
+
+  // Check sheet name and get the index
+  let sheet = sheets.filter(el => el.name === sheetName)
+  if (sheet.length === 0) {
+    return res.status(422).json({ msg: "Invalid sheet name" })
+  }
+
+  await promisify(doc.useServiceAccountAuth)(cred)
+
+  const info = await promisify(doc.getInfo)()
+  const worksheet = info.worksheets[sheet[0].index]
+  const rows = await promisify(worksheet.getRows)({ offset: 1 })
+
+  let stocks = []
+
+  rows.forEach(el => {
+    stocks.push({
+      no: el.no,
+      stock: el.stock,
+      close: el.close || el.hargaclose,
+      volume: el.volume,
+      value: el.value
+    })
+  })
+
+  let result = stocks.filter(el => el.stock !== "")
+
+  res.json({
+    message: "Successfull getting stocks data",
+    length: result.length,
+    data: result
   })
 })
 
