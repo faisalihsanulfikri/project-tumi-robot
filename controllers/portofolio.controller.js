bf328905b9b46cceaaad7bd352489b450325db55
william maulana yusuf <williammaulanayusuf1@gmail.com>
2019-11-08 15:01:05 +0700

    scraping portofolio

--- controllers/portofolio.controller.js
+++ controllers/portofolio.controller.js
@@ -1,6 +1,11 @@
 const { to, ReE, ReS } = require("../services/util.service")
 const { Portofolio } = require("../models")
 const { Stock } = require("../models")
+const { User } = require("../models");
+const { Security } = require("../models");
+const { Robot } = require("../models");
+const { Master_Setting } = require("../models");
+const { User_Setting } = require("../models");
 
 
 // get protofolio
@@ -139,13 +144,73 @@ module.exports.portofolio = async function(req,res){
     return data
 };
 
+async function getUsers() {
+  let userData, securityData, robotData;
+
+  [err, userData] = await to(User.findAll({ raw: true }));
+  [err, securityData] = await to(Security.findAll({ raw: true }));
+  [err, robotData] = await to(Robot.findAll({ raw: true }));
+  [err, m_setting_data] = await to(Master_Setting.findAll({ raw: true }));
+  [err, u_setting_data] = await to(User_Setting.findAll({ raw: true }));
+
+  let data = [];
+  let config_name = "";
+
+  robotData.forEach((rd, i) => {
+    // get user
+    let filter_user = userData.filter(ud => {
+      return ud.id == rd.user_id;
+    });
+    // get security
+    let filter_security = securityData.filter(sd => {
+      return sd.id == rd.security_id;
+    });
+    // get setting
+    let filter_setting = u_setting_data.filter(usd => {
+      return usd.user_id == rd.user_id;
+    });
+
+    // filter setting
+    let dataSetting = {};
+    filter_setting.forEach(fs => {
+      m_setting_data.forEach(msd => {
+        if (msd.id == fs.master_setting_id) {
+          config_name = msd.config_name;
+        }
+      });
+      dataSetting[config_name] = fs.config_value;
+    });
+
+    data[i] = {
+      user_id: filter_user[0].id,
+      security_user_id: filter_security[0].username,
+      username: filter_user[0].username,
+      email: filter_user[0].email,
+      phone: filter_user[0].phone,
+      password: filter_security[0].password,
+      pin: filter_security[0].pin,
+      active_date: filter_security[0].active_date,
+      expire_date: filter_security[0].expire_date,
+      user_status: filter_user[0].status,
+      robot_status: rd.status,
+      setting: dataSetting
+    };
+  });
+
+  let filter_data = data.filter(d => {
+    return d.user_status == "active" && d.robot_status == "on";
+  });
+
+  return filter_data;
+}
+
 module.exports.inputPortofolio = async function(req, res){
     const puppeteer = require("puppeteer");
     const { Portofolio } = require("../models")
 
     let portofolio,getPortofolio,err;
 
-    getPortofolio = await module.exports.getPortofolio();
+    getPortofolio = await module.exports.portofolio();
 
     const browser = await puppeteer.launch({
         headless: true,
@@ -155,8 +220,15 @@ module.exports.inputPortofolio = async function(req, res){
       const page = await browser.newPage();
       await page.waitFor(1000);
 
+      let users = await getUsers();
+
+      let thisUser = users[0];
+
+      let user_id = thisUser.user_id;
+
     getPortofolio.forEach(async el => {
         console.log(el)
+        el.user_id = user_id;
         [err, portofolio] = await to(Portofolio.create(el));
         
     });
