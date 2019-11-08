e809a6c4ec772f4c6f517877dc4dad644884ea13
william maulana yusuf <williammaulanayusuf1@gmail.com>
2019-11-08 14:39:45 +0700

    fix input user id

--- controllers/transaction.controller.js
+++ controllers/transaction.controller.js
@@ -1,6 +1,11 @@
 const { to, ReE, ReS } = require("../services/util.service")
 const { Transaction } = require("../models")
 const { Stock } = require("../models")
+const { User } = require("../models");
+const { Security } = require("../models");
+const { Robot } = require("../models");
+const { Master_Setting } = require("../models");
+const { User_Setting } = require("../models");
 
 module.exports.get_transaction = async function(req, res) {
   let transaction
@@ -25,8 +30,8 @@ module.exports.buyAndSell = async function(req, res) {
   const puppeteer = require("puppeteer")
   const Tesseract = require("../node_modules/tesseract.js")
   const fs = require("fs")
-  const username = "Rvr2492"
-  const password = "147903"
+  const username = "HE00201"
+  const password = "245625"
 
   const browser = await puppeteer.launch({
     headless: false,
@@ -111,7 +116,6 @@ module.exports.buyAndSell = async function(req, res) {
             result["match_amount"] = row[index].cells[11].textContent
             result["validity"] = row[index].cells[12].textContent
             result["channel"] = row[index].cells[13].textContent
-            result["user_id"] = req.params.user_id
           }
 
           items.push(result)
@@ -126,6 +130,67 @@ module.exports.buyAndSell = async function(req, res) {
     return (err)
   }
 }
+
+async function getUsers() {
+  let userData, securityData, robotData;
+
+  [err, userData] = await to(User.findAll({ raw: true }));
+  [err, securityData] = await to(Security.findAll({ raw: true }));
+  [err, robotData] = await to(Robot.findAll({ raw: true }));
+  [err, m_setting_data] = await to(Master_Setting.findAll({ raw: true }));
+  [err, u_setting_data] = await to(User_Setting.findAll({ raw: true }));
+
+  let data = [];
+  let config_name = "";
+
+  robotData.forEach((rd, i) => {
+    // get user
+    let filter_user = userData.filter(ud => {
+      return ud.id == rd.user_id;
+    });
+    // get security
+    let filter_security = securityData.filter(sd => {
+      return sd.id == rd.security_id;
+    });
+    // get setting
+    let filter_setting = u_setting_data.filter(usd => {
+      return usd.user_id == rd.user_id;
+    });
+
+    // filter setting
+    let dataSetting = {};
+    filter_setting.forEach(fs => {
+      m_setting_data.forEach(msd => {
+        if (msd.id == fs.master_setting_id) {
+          config_name = msd.config_name;
+        }
+      });
+      dataSetting[config_name] = fs.config_value;
+    });
+
+    data[i] = {
+      user_id: filter_user[0].id,
+      security_user_id: filter_security[0].username,
+      username: filter_user[0].username,
+      email: filter_user[0].email,
+      phone: filter_user[0].phone,
+      password: filter_security[0].password,
+      pin: filter_security[0].pin,
+      active_date: filter_security[0].active_date,
+      expire_date: filter_security[0].expire_date,
+      user_status: filter_user[0].status,
+      robot_status: rd.status,
+      setting: dataSetting
+    };
+  });
+
+  let filter_data = data.filter(d => {
+    return d.user_status == "active" && d.robot_status == "on";
+  });
+
+  return filter_data;
+}
+
 module.exports.inputTransaction  = async function(req, res){
   const puppeteer = require("puppeteer");
   const { Transaction } = require("../models")
@@ -143,19 +208,32 @@ module.exports.inputTransaction  = async function(req, res){
     });
 
     const page = await browser.newPage();
-    await page.waitFor(5000);
+    await page.waitFor(1000);
+    let users = await getUsers();
 
+    let thisUser = users[0];
+  
+    let user_id = thisUser.user_id;
+
+    let stock_id = 1;
 
     buyandsell.forEach(async el => {
+      el.user_id = user_id;
+      el.stock_id = stock_id;
         [err, transaction] = await to(Transaction.findOne({ where: { order_id: el.order_id } }));
         if (!transaction) {
           console.log(el);
           [err, transaction] = await to(Transaction.create(el));
+          if (err) return ReE(res, err, 422);
         }else{
           transaction.set(el);
           [err, transaction] = await to(transaction.save());
+          if (err) return ReE(res, err, 422);
         }
       });
+
+    await page.waitFor(2000);
+
 
       return ReS(res, { message: "Berhasil Input Transaksi" },201  );
 };
