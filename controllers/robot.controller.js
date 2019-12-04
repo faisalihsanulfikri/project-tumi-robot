b6298f7f9139e0df39e933a146224da771bcd4a6
faisal.ihsanulfikri <faisalihsanulfikri@gmail.com>
2019-12-04 10:03:36 +0700

    check-transaction

--- controllers/robot.controller.js
+++ controllers/robot.controller.js
@@ -247,12 +247,12 @@ async function automation(
     // SET / UPDATE DATA TO TUMI DATABASE
     await setTransactionData(page, user_id);
     await page.waitFor(5000);
-    await automationPortofolio(page, URL_protofolio, user_id);
-    await page.waitFor(5000);
-    await withdraws(page, URL_accountinfo, robot_id, user_id);
-    await page.waitFor(5000);
-    await inputStockRangking(page);
-    await page.waitFor(5000);
+    // await automationPortofolio(page, URL_protofolio, user_id);
+    // await page.waitFor(5000);
+    // await withdraws(page, URL_accountinfo, robot_id, user_id);
+    // await page.waitFor(5000);
+    // await inputStockRangking(page);
+    // await page.waitFor(5000);
 
     // TRANSACTION
     let transaction = await getTransaction(page);
