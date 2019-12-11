886fe680703d23872845dfdd5bd5236348dde62c
faisal.ihsanulfikri <faisalihsanulfikri@gmail.com>
2019-12-11 12:24:04 +0700

    fix sell times model

--- models/sell_times.model.js
+++ models/sell_times.model.js
@@ -1,24 +1,32 @@
 "use strict";
 module.exports = (sequelize, DataTypes) => {
   var Model = sequelize.define(
-    "Sell_Times",
+    "Stock_Sell",
     {
+      order_id: DataTypes.STRING,
       user_id: DataTypes.INTEGER,
-      on_sell_by_time: {
+      stock: DataTypes.STRING,
+      mode: DataTypes.STRING,
+      lots: DataTypes.STRING,
+      status: DataTypes.STRING,
+      priceBuy: DataTypes.STRING,
+      priceSell: DataTypes.STRING,
+      on_sale: {
         type: DataTypes.ENUM,
         values: ["yes", "no"],
         defaultValue: "no"
       }
     },
-    {}
+    {
+      freezeTableName: true,
+      tableName: "stock_sells"
+    }
   );
-
   Model.associate = function(models) {
     Model.belongsTo(models.User, {
       foreignKey: "user_id",
       as: "User"
     });
   };
-
   return Model;
 };
